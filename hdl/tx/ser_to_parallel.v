3bd6e37db40ad3b37e51cf2e7aff35aea811b852
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2024-11-25 23:24:53 +0200

    Update ser_to_parallel.v

--- hdl/tx/ser_to_parallel.v
+++ hdl/tx/ser_to_parallel.v
@@ -1,28 +1,28 @@
 module ser_to_parallel(
-    input clk,
-    input rst,
-    output [9:0] op,
-    input ip
+    input BitCLK,
+    input Reset,
+    output [9:0] RxParallel_10,
+    input Serial
 );
     reg [9:0] TR; // 10-bit shift register
     reg [3:0] count; // 4-bit counter
 
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
+    always @(posedge BitCLK or posedge Reset) begin
+        if (Reset) begin
             count <= 4'b0000; // Reset the counter
             TR <= 10'b0; // Reset the shift register
         end else begin
             case (count)
-                4'b0000: TR[0] <= ip;
-                4'b0001: TR[1] <= ip;
-                4'b0010: TR[2] <= ip;
-                4'b0011: TR[3] <= ip;
-                4'b0100: TR[4] <= ip;
-                4'b0101: TR[5] <= ip;
-                4'b0110: TR[6] <= ip;
-                4'b0111: TR[7] <= ip;
-                4'b1000: TR[8] <= ip;
-                4'b1001: TR[9] <= ip;
+                4'b0000: TR[0] <= Serial;
+                4'b0001: TR[1] <= Serial;
+                4'b0010: TR[2] <= Serial;
+                4'b0011: TR[3] <= Serial;
+                4'b0100: TR[4] <= Serial;
+                4'b0101: TR[5] <= Serial;
+                4'b0110: TR[6] <= Serial;
+                4'b0111: TR[7] <= Serial;
+                4'b1000: TR[8] <= Serial;
+                4'b1001: TR[9] <= Serial;
                 default: ; // No operation for other cases
             endcase
             
@@ -34,5 +34,5 @@ module ser_to_parallel(
         end
     end
 
-    assign op = TR; // Output the 10-bit value
+    assign RxParallel_10 = TR; // Output the 10-bit value
 endmodule
