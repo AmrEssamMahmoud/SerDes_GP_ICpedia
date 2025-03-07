b81fca41fac9cb1dc5cab3dcfe109370cdba1adc
OmarSalah239 <you@example.com>
2025-03-07 17:07:35 +0200

    update channel

--- hdl/designs/channel and equalizer/channel.sv
+++ hdl/designs/channel and equalizer/channel.sv
@@ -1,20 +1,17 @@
-module channel #(
-    parameter real T = 1.0e-12,  // Sampling period
-    parameter real TAU = 200.0e-12 // Time constant (TAU = 1/w_c)
-) (
+module channel(
     input clk,               // Clock signal
     input real channel_in,   // Input signal
     output real channel_out  // Filtered output signal
 );
-
-    real alpha = T / (T + TAU); // Filter coefficient
-    real reg_out = 0.0;         // Internal state to store the previous output
+    real out_prev = 0.0;         // Internal state to store the previous output
+    real in_prev = 0.0;           // Internal state to store the previous input   
 
     always @(posedge clk) begin
-        // y[n] = alpha * x[n] + (1 - alpha) * y[n-1]
-        reg_out <= alpha * channel_in + (1.0 - alpha) * reg_out;
+        // y[n] = 0.2696 * u[n-1] + 0.7304 * y[n-1]
+        out_prev <= 0.2696 * in_prev + 0.7304 * out_prev;
+        in_prev <= channel_in;
     end
 
-    assign channel_out = reg_out;
+    assign channel_out = out_prev;
 
 endmodule
